8dfc1cef1f81a3c0e181ae052aa74c6d4553cacc
Sudheesh-0007 <sudheeshchandra8@gmail.com>
2026-03-18 14:58:12 +0530

    Email validation is done

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render,redirect
 from .form import RegistrationForm
 from.models import Account
-from django.contrib import messages
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+# VARIFICATION EMAIL PROCESS
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 def register(request):
     if request.method =="POST":
@@ -18,8 +28,23 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number=phone_number
             user.save()
-            messages.success(request, "Registration Successful ")
-            return redirect('register')
+
+        # USER ACTIVATIONS
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account' 
+            message      = render_to_string('accounts/account_varification_email.html',{
+                'user'  : user,
+                'domain': current_site,
+        # encoding the primary key for nobody can see the primary key        
+                'uid'   : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token'  : default_token_generator.make_token(user),
+
+            })
+            to_email   = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
+            #messages.success(request, "Thank you for registering with us . we have sent you a varification email to your email address")
+            return redirect('/accounts/login/?command=verification&email='+email)
 
     else:
         form= RegistrationForm()
@@ -32,7 +57,41 @@ def register(request):
     return render(request ,'accounts/register.html',context)
 
 def login(request):
-    return render(request ,'accounts/login.html')
+    if request.method =="POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email , password=password)
+        if user is not None:
+            auth.login(request ,user)
+            # messages.success(request,"You are now logged in.")
+            return redirect('home')
+        else :
+            messages.error(request, "Invalid Login Credentials")
+            return redirect('login')
 
+    return render(request ,'accounts/login.html')
+ 
+@login_required(login_url='login')
 def logout(request):
-    return render(request ,'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request,"You are Logged Out")
+
+    return redirect('login')
+
+def activate(request,uidb64,token):
+    try:
+        uid  = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        messages.success(request, 'Congratulations ! Your account is activated.')
+        return redirect('login')
+    else :
+        messages.error(request, "Invalid activation link")
+        return redirect('register')
+    
